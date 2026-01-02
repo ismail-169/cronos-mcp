787612a8ccba3d56c0152e7c8fa72deed606da43
ismail-169 <oganlaismail305@gmail.com>
2026-01-02 17:27:40 +0100

    added default dirname

--- apps/registry/src/index.ts
+++ apps/registry/src/index.ts
@@ -5,6 +5,10 @@
 import http from 'http';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // =============================================================================
 // Types
